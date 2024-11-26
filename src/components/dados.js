376de100835fabb11c68f47d7691ada6bd17fcd7
matheuslufano <matheuslufano93@gmail.com>
2024-11-26 18:29:12 -0300

    criei um arquivo chamado dados

---
+++ src/components/dados.js
@@ -0,0 +1,42 @@
+// src/dados.js
+const dados = [
+    {
+        title: "Campanha contra a fome - Ação Cidadania",
+        date: "2024-11-20",
+        content: "A ONG Ação Cidadania está promovendo uma campanha nacional para arrecadar alimentos e combater a fome em comunidades carentes. Mais de 500 toneladas de alimentos já foram distribuídas, mas a meta é alcançar 1.000 toneladas até o final do ano. Participe e faça a diferença!",
+        image: "https://observatorio3setor.org.br/wp-content/uploads/2023/10/fotos-dimensionadas-p-site-9-1.png",
+    },
+    {
+        title: "Proteção de Animais - SOS Mata Atlântica",
+        date: "2024-11-15",
+        content: "A SOS Mata Atlântica lançou uma nova iniciativa para proteger a fauna ameaçada de extinção na região do bioma Mata Atlântica. O projeto busca voluntários para monitoramento de espécies e doações para a construção de novos abrigos naturais.",
+        image: "https://sosma.org.br/storage/media/An0jjQaRxVLCP5RzejU2ycSPAnNP37VbNxwEBGxG.png",
+    },
+    {
+        title: "Educação para Todos - Instituto Ayrton Senna",
+        date: "2024-10-30",
+        content: "O Instituto Ayrton Senna anunciou uma parceria com redes municipais de ensino para implementar programas de desenvolvimento socioemocional em 300 escolas pelo Brasil. O objetivo é melhorar o desempenho acadêmico e reduzir a evasão escolar.",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTutOJuLVONI33HvHdJe40SupILSZtDGNU3sg&s",
+    },
+    {
+        title: "Reflorestamento da Amazônia - WWF Brasil",
+        date: "2024-11-01",
+        content: "A WWF Brasil está liderando um projeto de reflorestamento na Amazônia, com foco na recuperação de áreas degradadas e promoção de agricultura sustentável. Já foram plantadas mais de 1 milhão de mudas, beneficiando comunidades locais e protegendo o ecossistema.",
+        image: "https://wwfbrnew.awsassets.panda.org/img/original/original_ww1275302.jpg",
+    },
+    {
+        title: "Direitos das Mulheres - Instituto Maria da Penha",
+        date: "2024-10-25",
+        content: "O Instituto Maria da Penha está organizando um seminário sobre os direitos das mulheres no Brasil, com foco na prevenção da violência doméstica e no fortalecimento de políticas públicas. O evento contará com palestras de especialistas e autoridades.",
+        image: "https://jornalistainclusivo.com/wp-content/uploads/2021/03/Foto-Maria-da-Penha-Cid-Moreira-Fotografias-JI.jpg",
+    },
+    {
+        title: "Apoio à Saúde Infantil - Médicos Sem Fronteiras",
+        date: "2024-11-10",
+        content: "Médicos Sem Fronteiras lançou uma campanha para angariar fundos e levar atendimento médico a crianças em situação de vulnerabilidade em regiões de conflito. A cada doação, vidas estão sendo salvas com medicamentos, vacinas e cuidados essenciais.",
+        image: "https://www.msf.org.br/wp-content/uploads/2024/03/MSB169913_Medium.jpg",
+    },  
+];
+
+
+export default dados;
